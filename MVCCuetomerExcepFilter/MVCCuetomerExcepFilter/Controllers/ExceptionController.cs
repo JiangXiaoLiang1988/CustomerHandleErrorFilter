ea88c66926df47448879f3f85b02cbeeee8ec574
jiangxiaoliang1988 <jiangxiaoliang@163.com>
2019-05-11 11:07:50 +0800

    commit MVC customer HandleErrorFilter code

---
+++ MVCCuetomerExcepFilter/MVCCuetomerExcepFilter/Controllers/ExceptionController.cs
@@ -0,0 +1,25 @@
+﻿using MVCCuetomerExcepFilter.Extension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCuetomerExcepFilter.Controllers
+{
+    public class ExceptionController : Controller
+    {
+        // GET: Exception
+        /// <summary>
+        /// View表示发生异常时指定的视图
+        /// 这里表示发生异常时使用ExceptionDetails视图
+        /// </summary>
+        /// <returns></returns>
+        [ExceptionFilters(View =("ExceptionDetails"))]
+        public ActionResult Index()
+        {
+            // 测试抛出异常
+            throw new NullReferenceException("测试抛出的异常");
+        }
+    }
+}
---
+++ MVCCuetomerExcepFilter/MVCCuetomerExcepFilter/Extension/ExceptionFilters.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCuetomerExcepFilter.Extension
+{
+    /// <summary>
+    /// 异常过滤器
+    /// </summary>
+    public class ExceptionFilters : HandleErrorAttribute
+    {
+        /// <summary>
+        /// 在异常发生时调用
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnException(ExceptionContext filterContext)
+        {
+            // 判断是否已经处理过异常
+            if(!filterContext.ExceptionHandled)
+            {
+                // 获取出现异常的controller和action名称，用于记录
+                string strControllerName = filterContext.RouteData.Values["controller"].ToString();
+                string strActionName = filterContext.RouteData.Values["action"].ToString();
+                // 定义一个HandleErrorInfo,用于Error视图展示异常信息
+                HandleErrorInfo info = new HandleErrorInfo(filterContext.Exception, strControllerName, strActionName);
+
+                ViewResult result = new ViewResult
+                {
+                    ViewName = this.View,
+                    // 定义ViewData，泛型
+                    ViewData = new ViewDataDictionary<HandleErrorInfo>(info)
+                };
+                // 设置操作结果
+                filterContext.Result = result;
+                // 设置已经处理过异常
+                filterContext.ExceptionHandled = true;
+            }
+            //base.OnException(filterContext);
+        }
+    }
+}
